c6859bf99f28246ef1c2dad5fd206ba58f9fd75c
vanbugillord <vanbugillord@gmail.com>
2023-07-24 13:42:52 +0700

    new file:   application/views/Perusahaan/profile.php 	new file:   application/views/Perusahaan/profile_update.php 	new file:   upload/dokumen/fd7416347fe8e5da4991208c760d18ae.png     modified:   application/controllers/Perusahaan.php

--- application/controllers/Perusahaan.php
+++ application/controllers/Perusahaan.php
@@ -9,6 +9,20 @@ class Perusahaan extends CI_Controller {
       $this->load->library('form_validation');
       $this->load->library('upload');
       $this->load->library('uuid');
+      $this->load->model([
+        'Sekolah_model',
+        'Siswa_model',
+        'Perusahaan_model',
+        'Bdperusahaan_model',
+        'Lowongan_model',
+        'Posisi_model',
+        'PsLowongan_model',
+        'Swpengalaman_model',
+        'File_model',
+        'Lamaran_model',
+        'Permintaan_model',
+        'User_model'
+      ]);
       is_logged_in();
     }
     
@@ -19,4 +33,95 @@ class Perusahaan extends CI_Controller {
         );
         $this->template->load('template', 'Perusahaan/dashboard', $data);
 	}
+
+  public function profile()
+  {
+
+    $uper = $this->session->userdata('user_id');
+    
+    $row = $this->Perusahaan_model->get_by_id($uper);
+
+    $data = array(
+      'title' => 'Admin Area - Detail Perusahaan',
+      'id' => $row->id,
+      'uper' => $uper,
+      'nama_perusahaan' => $row->nama_perusahaan,
+      'alamat' => $row->alamat,
+      'jumlah_karyawan' => $row->jumlah_karyawan,
+      'deskripsi' => $row->deskripsi,
+      'id_user' => $row->id_user,
+    );
+    $this->template->load('template', 'Perusahaan/profile', $data);
+    
+  }
+
+  public function profile_update()
+  {
+
+    $uper = $this->session->userdata('user_id');
+    
+    $row = $this->Perusahaan_model->get_by_id($uper);
+
+    $data = array(
+      'title' => 'Admin Area - Form Data Perusahaan',
+      'button' => 'Update',
+      'action' => site_url('perusahaan/perusahaan_update_action'),
+      'id' => set_value('id', $row->id),
+      'nama_perusahaan' => set_value('nama_perusahaan', $row->nama_perusahaan),
+      'alamat' => set_value('alamat', $row->alamat),
+      'jumlah_karyawan' => set_value('jumlah_karyawan', $row->jumlah_karyawan),
+      'deskripsi' => set_value('deskripsi', $row->deskripsi),
+      'id_user' => set_value('id_user', $row->id_user),
+      'uuid' => set_value('uuid', $row->uuid),
+    );
+    $this->template->load('template', 'Perusahaan/profile_update', $data);
+    
+  }
+
+  public function perusahaan_update_action()
+  {
+    $this->perusahaan_rules();
+
+    if ($this->form_validation->run() == FALSE) {
+      $this->profile_update($this->input->post('id', TRUE));
+    } else {
+      $data = array(
+        'nama_perusahaan' => $this->input->post('nama_perusahaan', TRUE),
+        'alamat' => $this->input->post('alamat', TRUE),
+        'jumlah_karyawan' => $this->input->post('jumlah_karyawan', TRUE),
+        'deskripsi' => $this->input->post('deskripsi', TRUE),
+        'id_user' => $this->input->post('id_user', TRUE),
+      );
+
+      $this->Perusahaan_model->update($this->input->post('id', TRUE), $data);
+      $fileuploaded = array();
+      if (!empty($_FILES['foto']['name'])) :
+        $fileuploaded =  upload_files('upload/dokumen', $this->input->post('uuid', TRUE), $_FILES['foto'], $this->input->post('id', TRUE), 6, 'foto[]');
+      endif;
+
+      $bidang_list = $this->input->post('bidang_list');
+      $this->Bdperusahaan_model->delete_perusahaan($this->input->post('id', TRUE));
+      if (!empty($bidang_list)) :
+        foreach ($bidang_list as $bidang) :
+          $datab = array(
+            'id_perusahaan' => $this->input->post('id', TRUE),
+            'id_bidang' => $bidang,
+          );
+          $this->Bdperusahaan_model->insert($datab);
+        endforeach;
+      endif;
+
+      $this->session->set_flashdata('message', 'Update Record Success');
+      redirect(site_url('perusahaan/profile'));
+    }
+  }
+
+  public function perusahaan_rules()
+  {
+    $this->form_validation->set_rules('nama_perusahaan', 'Nama Perusahaan', 'trim|required');
+    $this->form_validation->set_rules('jumlah_karyawan', 'Jumlah Karyawan', 'trim|required');
+
+    $this->form_validation->set_rules('id', 'id', 'trim');
+    $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+  }
 }
---
+++ application/views/Perusahaan/profile_update.php
@@ -0,0 +1,100 @@
+<!-- Main content -->
+<div class='row'>
+  <div class='col-12'>
+    <div class='box'>
+      <div class='box-header'>
+
+        <h2 class='box-title'>Form Data Perusahaan</h2>
+        <br>
+        <div class='box box-primary'>
+
+          <form action="<?php echo $action; ?>" class="form-underline" style="max-width: 600px !important;" method="post"  enctype="multipart/form-data">
+          <div class="field-input mt-2">
+              <label for="pengalaman">Foto Profil
+                <?php echo form_error('foto') ?></label>
+              <div class="row" id="fotoprofil">
+                <?php $foto = getfotoprofil($id,6);
+                if (!empty($foto)) : ?>
+                  <div class="col-3" id="inputFormRow">
+                    <div class="field-group field-file">
+                      <label for="foto" class="preview">
+                        <img class="img_preview" src="<?= base_url() . "upload/dokumen/" . $foto->file ?>" alt="">
+                        <i class="la la-cloud-upload-alt"></i>
+                      </label>
+                      <div class="field-note"><a href="<?= base_url('admin/siswa_delete_file') ?>" data-id="<?= $foto->id ?>" class="btn-sm btn-danger gantiFile"> <i class="la la-exchange-alt"></i> Ganti Foto</a></div>
+                    </div>
+                  </div>
+                <?php else : ?>
+                  <div class="col-3">
+                    <div class="field-group field-file">
+                      <label for="foto" class="preview">
+                        <input type="file" id="foto" name="foto[]" class="upload-file" data-max-size="1024">
+                        <img class="img_preview" src="" alt="">
+                        <i class="la la-cloud-upload-alt"></i>
+                      </label>
+                      <div class="field-note">Maximum file size: 1 MB.</div>
+                    </div>
+                  </div>
+                <?php endif; ?>
+              </div>
+            
+            </div>
+          <div class="field-input">
+              <input type="hidden" name="id" value="<?php echo $id; ?>" />
+              <input type="hidden" name="uuid" value="<?php echo $uuid; ?>" />
+              <label for="nama_perusahaan">Nama Perusahaan <?php echo form_error('nama_perusahaan') ?></label>
+              <input type="text" name="nama_perusahaan" placeholder="Nama Perusahaan" id="nama_perusahaan" value="<?php echo $nama_perusahaan; ?>">
+            </div>
+            <div class="field-input">
+              <label for="alamat">Alamat <?php echo form_error('alamat') ?></label>
+              <input type="text" name="alamat" placeholder="Alamat" id="alamat" value="<?php echo $alamat; ?>">
+            </div>
+            <div class="field-input">
+              <label for="alamat">Jumlah Karyawan <?php echo form_error('jumlah_karyawan') ?></label>
+              <input type="number" name="jumlah_karyawan" placeholder="0" id="jumlah_karyawan" value="<?php echo $jumlah_karyawan; ?>">
+            </div>
+            <div class="field-input">
+              <label class="mb-2" for="bidang">Bergerak Dibidang : <?php echo form_error('bidang') ?></label>
+              <?php
+              $no = 1;
+              $arr = array();
+              $cekbidang = loadbidangperusahaan($id);
+              foreach($cekbidang as $ck):
+                array_push($arr,$ck->id_bidang);
+              endforeach;
+              foreach (loadbidang() as $bidang) : ?>
+               <div class="field-check">
+											<label for="bidang_list<?=$no?>">
+												<input <?= in_array($bidang->id,$arr) ? 'checked' : '' ?> name="bidang_list[]"  id="bidang_list<?=$no?>" type="checkbox" value="<?= $bidang->id ?>">
+												<span class="checkmark">
+													<i class="la la-check"></i>
+												</span>
+                        <?= $bidang->nama ?>
+											</label>
+										</div>
+              <?php
+                $no++;
+              endforeach; ?>
+
+            </div>
+            <div class="field-input mt-2">
+              <label for="Deskripsi">Deskripsi <?php echo form_error('deskripsi') ?></label>
+              <textarea name="deskripsi" id="" cols="30" rows="10"><?php echo $deskripsi; ?></textarea>
+            </div>
+            <div class="field-submit mt-3">
+              <button type="submit" class="btn btn-primary"><?php echo $button ?></button>
+              <a href="#" onclick="history.back()" class="btn btn-danger">Cancel</a>
+            </div>
+          </form>
+        </div><!-- /.box-body -->
+      </div><!-- /.box -->
+    </div><!-- /.col -->
+  </div><!-- /.row -->
+
+  <script>
+    $(document).ready(function() {
+      CKEDITOR.replace('deskripsi', {
+        width: '100%'
+      });
+    });
+  </script>
